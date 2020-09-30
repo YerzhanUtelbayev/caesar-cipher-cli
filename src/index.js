d1019f66aeba5457ddc38d31211a8c49290bbb53
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-09-30 14:17:40 +0600

    feat: add decoding method

--- src/index.js
+++ src/index.js
@@ -28,3 +28,32 @@ const encode = (inputString, inputNumber = 1) => {
 
   return encodedList.join('');
 };
+
+const decode = (inputString, inputNumber = 1) => {
+  if (typeof inputString !== 'string') return;
+
+  const alphabet = alphabetString.split('');
+  const lastElIndex = alphabet.length - 1;
+  const shiftNumber = inputNumber % lastElIndex;
+  const inputSymbolsList = inputString.split('');
+
+  const decodedList = inputSymbolsList.map((value) => {
+    const lowCaseValue = value.toLowerCase();
+    const alphabetIndex = alphabet.indexOf(lowCaseValue);
+
+    if (alphabetIndex === -1) return lowCaseValue;
+
+    const shiftedIndex = alphabetIndex - shiftNumber;
+
+    const decodedElIndex =
+      shiftedIndex >= 0 ? shiftedIndex : lastElIndex - shiftedIndex;
+
+    if (value === lowCaseValue) {
+      return alphabet[decodedElIndex];
+    } else {
+      return alphabet[decodedElIndex].toUpperCase();
+    }
+  });
+
+  return decodedList.join('');
+};
